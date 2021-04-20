0568f6fe1a3e65f8c310af8e2232f61a09cf782c
Ritik Gupta <rittikgupta1@gmail.com>
2021-04-20 20:42:18 +0530

    Edited .gitignore

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { static } = require("express");
 const app = express();
+const mongoose =require("mongoose")
+
+
+
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 var items = ["Eat food", "Cook Food", "Buy Food"];
