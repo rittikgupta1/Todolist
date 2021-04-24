558adf9d1609955f7fe9ac837b9715216335c36f
Ritik Gupta <rittikgupta1@gmail.com>
2021-04-24 10:59:59 +0530

    Ready to deploy

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const { static } = require("express");
 const app = express();
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://Admin_rittik:<pass>@cluster0.vjxlj.mongodb.net/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const itemSchema = new mongoose.Schema({
     name: String
@@ -122,7 +122,7 @@ app.get("/:customListName", (req, res) => {
     })
 });
 ///Listening on port 5000
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Server started on port 5000");
 })
 
